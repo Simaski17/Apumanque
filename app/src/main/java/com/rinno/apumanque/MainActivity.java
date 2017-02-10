b0be80d54f5ba89f1a443f154fdbdec6564c351a
simaski <jimmyhernandezu@gmail.com>
2017-02-10 17:37:13 -0300

    RESOLVI ESA MIERDA

--- app/src/main/java/com/rinno/apumanque/MainActivity.java
+++ app/src/main/java/com/rinno/apumanque/MainActivity.java
@@ -41,24 +41,17 @@ public class MainActivity extends AppCompatActivity {
 
 
         DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
-        //DatabaseReference  dbApumanqueNodes = reference.child("nodes");
-        //DatabaseReference  dbApumanqueEdges = reference.child("Edges");
 
         reference.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                //Log.e("TAG","VALOR: "+dataSnapshot.child("Nodes").getChildrenCount());
 
                 for(int i = 1; i <= dataSnapshot.child("Nodes").getChildrenCount(); i++) {
 
                     Nodes nod = dataSnapshot.child("Nodes").child(String.valueOf(i)).getValue(Nodes.class);
                     Graph.Vertex<String> a = new Graph.Vertex<String>(nod.getId().trim());
-                    //Log.e("TAG","VALOR A: "+nod.getId().getClass().toString());
                     vertices.add(a);
-                   //Log.e("TAG","VALOR A: "+a);
-
                 }
-                //Log.e("TAG","VALOR ed: "+vertices);
 
                 for(int i = 1; i <= dataSnapshot.child("Edges").getChildrenCount(); i++) {
 
@@ -68,29 +61,17 @@ public class MainActivity extends AppCompatActivity {
 
                     for (int l = 0; l < vertices.size(); l++){
                         if(vertices.get(l).equals(a)){
-                            //Log.e("TAG","ENCONTRADO A: "+vertices.get(l)+ i + "---> " +l);
                             arregloA.add(l);
                         }
                     }
 
                     for (int l = 0; l < vertices.size(); l++){
                         if(vertices.get(l).equals(b)){
-                            //Log.e("TAG","ENCONTRADO B: "+vertices.get(l)+ i);
                             arregloB.add(l);
                         }
                     }
                     arregloCosto.add(edg.getCosto());
-                    //Log.e("TAG","arregloA: "+arregloA);
-                    //Log.e("TAG","arregloB: "+arregloB);
-                    //Log.e("TAG","arregloCosto: "+arregloCosto);
-                    //Graph.Edge<String> ed = new Graph.Edge<String>(edg.getCosto(),a,b);
-                    //edges.add(ed);
                 }
-                //Log.e("TAG","Vertices: "+vertices);
-
-
-
-
             }
 
             @Override
@@ -99,55 +80,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-//        dbApumanqueNodes.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//
-//
-//                for(int i = 1; i < dataSnapshot.getChildrenCount(); i++) {
-//
-//                    Nodes nod = dataSnapshot.child(String.valueOf(i)).getValue(Nodes.class);
-//                    //Log.e("TAG","VALOR: "+nod.getId());
-//
-//                    //Log.e("TAG", "FIREBASE " + dataSnapshot.child(String.valueOf(i)).getValue().toString());
-//                    Graph.Vertex<String> a = new Graph.Vertex<String>(nod.getId());
-//                    vertices.add(a);
-//
-//                }
-//
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//                Log.e("TAG","ERROR "+databaseError);
-//            }
-//        });
-
-//        dbApumanqueEdges.addValueEventListener(new ValueEventListener() {
-//            @Override
-//            public void onDataChange(DataSnapshot dataSnapshot) {
-//                for(int i = 1; i < dataSnapshot.getChildrenCount(); i++) {
-//
-//                    Edges edg = dataSnapshot.child(String.valueOf(i)).getValue(Edges.class);
-//                    Log.e("TAG","VALOR: "+edg.getCost());
-//
-//                    //Log.e("TAG", "FIREBASE " + dataSnapshot.child(String.valueOf(i)).getValue().toString());
-////                    Graph.Vertex<String> a = new Graph.Vertex<String>(nod.getId());
-////                    vertices.add(a);
-//                    Graph.Vertex<String> a = new Graph.Vertex<String>(edg.getSource());
-//                    Graph.Vertex<String> b = new Graph.Vertex<String>(edg.getEnd());
-//                    Graph.Edge<String> edggg = new Graph.Edge<String>(edg.getCost(),  a, b);
-//                    edges.add(edggg);
-//
-//                }
-//            }
-//
-//            @Override
-//            public void onCancelled(DatabaseError databaseError) {
-//                Log.e("TAG","ERROR "+databaseError);
-//            }
-//        });
-//
         btPrueba = (Button) findViewById(R.id.btPrueba);
         btPrueba.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -160,13 +92,7 @@ public class MainActivity extends AppCompatActivity {
 
                 Graph<String> graph = new Graph<String>(Graph.TYPE.UNDIRECTED, vertices, edges);
                 Astar astar=new Astar();
-        //astar.aStar(graph,   a, d);
-        Log.e("TAG","RUTA: "+astar.aStar(graph, vertices.get(0), vertices.get(4)));
-
-
-                //Log.e("TAG","GRAFO: "+graph);
-                //Log.e("TAG","VERTICES: "+vertices);
-                //Log.e("TAG","VERTICES: "+edges);
+                Log.e("TAG","RUTA: "+astar.aStar(graph, vertices.get(0), vertices.get(4)));
             }
         });
 
